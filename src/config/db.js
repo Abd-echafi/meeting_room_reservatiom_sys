a02964856ef2347a494f035ba526bc6f7cd6d5df
Abd-echafi <abdechafifilali@gmail.com>
2025-05-21 21:53:07 +0100

    db host

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false, // Set true to enable SQL logging
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false, // Set true to enable SQL logging
+//   }
+// );
+
+
+const sequelize = new Sequelize(process.env.RAILWAY_URI, {
+  dialect: 'mysql',
+  logging: false,
+});
 
 module.exports = sequelize;
