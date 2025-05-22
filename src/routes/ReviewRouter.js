a95e623791e37b268252f68dd538217c7a812722
Abd-echafi <abdechafifilali@gmail.com>
2025-05-22 19:09:46 +0100

    nodemon off + admin delete reviews permission

--- src/routes/ReviewRouter.js
+++ src/routes/ReviewRouter.js
@@ -4,6 +4,6 @@ const { protect, restrictTo } = require('../controllers/authenticationController
 const { deleteReview, updateReview, getReviewById, getAllReviewsForRoom, createReview } = require('../controllers/ReviewControllers');
 Router.route("/").post(protect, restrictTo('Client'), createReview);
 Router.route("/rooms/:room_id").get(getAllReviewsForRoom);
-Router.route("/:id").get(getReviewById).patch(protect, restrictTo("Client"), updateReview).delete(protect, restrictTo("Client"), deleteReview);
+Router.route("/:id").get(getReviewById).patch(protect, restrictTo("Client"), updateReview).delete(protect, restrictTo("Admin", "Client"), deleteReview);
 
 module.exports = Router;
